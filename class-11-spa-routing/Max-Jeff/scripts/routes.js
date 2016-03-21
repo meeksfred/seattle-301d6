94ff87541538749bd7efbf80cf284aad59ed29f4
Jeff Eng <jeff@jeffeng.me>
2016-03-21 13:23:55 -0700

    finished mvp todos on pair assignment

--- class-11-spa-routing/Max-Jeff/scripts/routes.js
+++ class-11-spa-routing/Max-Jeff/scripts/routes.js
@@ -1,6 +1,6 @@
 // Done: Configure our two routes for this app with page.js, by registering each URL your app can handle,
 // linked to a a single controller function to handle it:
-page('/', articleController.index);
+page('/', articlesController.index);
 page('/about', aboutController.index);
 // Done: What function do we call to activate page.js? Fire it off now, to execute
 page();
